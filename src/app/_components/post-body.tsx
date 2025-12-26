38937673ac39a798027fd5716c8d76591ed40561
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2025-12-26 10:07:26 -0500

    Simplifies markdown formatting approach

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,16 +1,12 @@
-import markdownStyles from "./markdown-styles.module.css";
-
 type Props = {
   content: string;
 };
 
 export function PostBody({ content }: Props) {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
+    <div
+      className="prose prose-lg max-w-2xl mx-auto"
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
   );
 }
